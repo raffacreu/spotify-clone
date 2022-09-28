49b88d7d77efd52deadaee297501eeb5861dba5b
raffacreu <rafaeu.rocha87@gmail.com>
2022-09-27 22:12:21 -0300

    feat: add auth login & crypt data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+//import React from "react";
+import Login from './Login';
+import { useEffect, useState } from 'react';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null)
+
+  // run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl()
+    window.location.hash = ''
+
+    const _token = hash.access_token
+
+    if(_token) {
+      setToken(_token)
+    }
+
+    console.log('I HAVE A TOKEN >>>', token)
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        token ? (
+          <h1>Estou Logado</h1>
+          ): (
+          <Login /> 
+          )
+      }
     </div>
   );
 }
