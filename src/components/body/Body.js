ed02a83b03ac7ae62978e2ea2e71355568152df2
raffacreu <rafaeu.rocha87@gmail.com>
2022-09-29 00:38:48 -0300

    feat: add listSongs

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,10 +1,46 @@
+import { Favorite, MoreHoriz, PlayCircleFilled } from '@mui/icons-material'
 import React from 'react'
 import { useDataLayerValue } from '../../context/DataLayer'
 import Header from '../header/Header'
+import SongRow from '../songrow/SongRow'
 import './Body.css'
 
 function Body ({ spotify }) {
   const [{ discovery_weekly }, dispatch] = useDataLayerValue()
+
+  const playPlaylist = (id) => {
+    spotify.play({
+      context_uri:`spotify:playlist:37i9dQZEVXcCpA0TBnOrqy`,
+    }).then((response) => {
+      spotify.getMyCurrentPlayingTrack().then((res) => {
+        dispatch({
+          type: 'SET_ITEM',
+          item: res.item
+        })
+        dispatch({
+          type:'SET_PLAYING',
+          playing: true,
+        })
+      })
+    })
+  }
+
+  const playSong = (id) => {
+    spotify.play({
+      uris:[`spotify:tracks:${id}`],
+    }).then((response) => {
+      spotify.getMyCurrentPlayingTrack().then((res) => {
+        dispatch({
+          type: 'SET_ITEM',
+          item: res.item,
+        })
+        dispatch({
+          type: 'SET_PLAYING',
+          playing: true,
+        })
+      })
+    })
+  }
 
   return (
     <div className='body'>
@@ -18,6 +54,18 @@ function Body ({ spotify }) {
             <h2>Descobertas da Semana</h2>
             <p>{discovery_weekly?.description}</p>
           </div>
+        </div>
+
+        <div className='body_songs'>
+        <div className='body_icons'>
+          <PlayCircleFilled className='body_shuffle' onClick={ playPlaylist } />
+          <Favorite fontSize='large' />
+          <MoreHoriz />
+        </div>  
+
+          {discovery_weekly?.tracks.items.map((item) => (
+            <SongRow playSong={ playSong } track={item.track} />
+          ))}
         </div>
     </div>
   )
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
     playing: false,
     item: null,
     // remove after finished developing....
-    token: 'BQB9fWrG5ebjujHA1zSX_ruQ5Hy_mLV3KHvjBJWIzmTkmW_1q3Zoui9PVFr5NLtlCelOdn_iQdr8DGyjEatlp-qxCGkzT_oQtwZOgL3qJ9YeKJ3-7ZY_LazUnNt9nfhyygna1skSm31XYdZVyN10kTeasQVbZ8ii_tuHca7xS3_U0rkwEZPsv78BMAWdH0keXuf0pJE',
+    // token: 'BQB9fWrG5ebjujHA1zSX_ruQ5Hy_mLV3KHvjBJWIzmTkmW_1q3Zoui9PVFr5NLtlCelOdn_iQdr8DGyjEatlp-qxCGkzT_oQtwZOgL3qJ9YeKJ3-7ZY_LazUnNt9nfhyygna1skSm31XYdZVyN10kTeasQVbZ8ii_tuHca7xS3_U0rkwEZPsv78BMAWdH0keXuf0pJE',
 }
 
 const reducer = (state, action) => {
