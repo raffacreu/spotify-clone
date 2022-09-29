dd1658ec34c3eed2bffe4d498a821aa5682eddd9
raffacreu <rafaeu.rocha87@gmail.com>
2022-09-29 20:18:37 -0300

    fix: SongRow in body

--- src/components/songrow/SongRow.js
+++ src/components/songrow/SongRow.js
@@ -5,7 +5,7 @@ function SongRow({ track }, playSong) {
   return (
     <div className="songRow" onClick={() => playSong(track.id)}>
       <img className="songRow__album" src={track.album.images[0].url} alt="" />
-      <div className="songRow__info">
+      <div className="songRow__info" >
         <h1>{track.name}</h1>
         <p>
           {track.artists.map((artist) => artist.name).join(", ")} -{" "}
